a1dc1bfc2c34c0edefddeb8f1600103b3606ce5a
DogukanSakin <dogukan.sakin@ftechlabs.com>
2024-03-04 21:55:53 +0300

    Initial Commit

---
+++ Programmatic-BMI-Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Programmatic-BMI-Calculator
+//
+//  Created by Doğukan Sakin on 4.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
