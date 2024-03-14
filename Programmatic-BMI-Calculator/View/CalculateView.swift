6365a33992b6718921889d527fdc118d54a1f7da
DogukanSakin <dogukan.sakin@ftechlabs.com>
2024-03-14 17:39:53 +0300

    Added custom slider component

--- Programmatic-BMI-Calculator/View/CalculateView.swift
+++ Programmatic-BMI-Calculator/View/CalculateView.swift
@@ -2,7 +2,56 @@
 //  CalculateView.swift
 //  Programmatic-BMI-Calculator
 //
-//  Created by Doğukan Sakin on 12.03.2024.
+//  Created by Doğukan Sakin on 4.03.2024.
 //
 
-import Foundation
+import UIKit
+
+protocol CalculateViewDelegate:AnyObject {
+    func calculateBMIButtonTapped(weight: Double, height: Double)
+}
+
+class CalculateView:UIView{
+    weak var delegate:CalculateViewDelegate?
+    
+    let heightSlider = CustomSlider(rightHeaderText: "Height", leftHeaderText: "180cm")
+    
+    let weightSlider = CustomSlider(rightHeaderText: "Weight", leftHeaderText: "80kg")
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViews()
+     
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupViews(){
+        let slidersContainer = UIStackView()
+        self.addSubview(slidersContainer)
+        slidersContainer.addArrangedSubview(heightSlider)
+        slidersContainer.addArrangedSubview(weightSlider)
+        slidersContainer.axis = .vertical
+        slidersContainer.spacing = 30
+        
+        slidersContainer.translatesAutoresizingMaskIntoConstraints = false
+        
+        
+        NSLayoutConstraint.activate([
+            slidersContainer.bottomAnchor.constraint(equalTo: self.bottomAnchor,constant: -120),
+            slidersContainer.leadingAnchor.constraint(equalTo: self.layoutMarginsGuide.leadingAnchor),
+            slidersContainer.trailingAnchor.constraint(equalTo: self.layoutMarginsGuide.trailingAnchor)
+            
+            
+        ])
+        
+    }
+    
+    
+    
+
+}
+
+
