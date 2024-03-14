cf64868225f45e3a411ca40d4fcaf6b926758a22
DogukanSakin <dogukan.sakin@ftechlabs.com>
2024-03-14 17:39:53 +0300

    Added custom slider component

--- Programmatic-BMI-Calculator/Components/CustomSlider.swift
+++ Programmatic-BMI-Calculator/Components/CustomSlider.swift
@@ -5,4 +5,55 @@
 //  Created by Doğukan Sakin on 14.03.2024.
 //
 
-import Foundation
+import UIKit
+
+class CustomSlider : UIStackView {
+    var rightHeaderText:String = ""
+    var leftHeaderText:String = ""
+    
+    private let slider = UISlider()
+    private let leftHeader = UILabel()
+    private let rightHeader = UILabel()
+    private let headerStackView = UIStackView()
+    
+    init(rightHeaderText: String, leftHeaderText: String) {
+        super.init(frame: .zero)
+        self.rightHeaderText = rightHeaderText
+        self.leftHeaderText = leftHeaderText
+        
+        // Container
+        self.addArrangedSubview(headerStackView)
+        self.addArrangedSubview(slider)
+        self.axis = .vertical
+        self.spacing = 20
+        
+        // Header
+        self.leftHeader.text = leftHeaderText
+        self.rightHeader.text = rightHeaderText
+        self.rightHeader.textAlignment = .right
+        self.headerStackView.addArrangedSubview(leftHeader)
+        self.headerStackView.addArrangedSubview(rightHeader)
+        self.headerStackView.distribution = .fillEqually
+        
+        // Slider
+        self.slider.thumbTintColor = UIColor(named: "Main")
+        self.slider.tintColor = .systemGray
+        self.translatesAutoresizingMaskIntoConstraints = false
+        
+    }
+    
+    required init(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+  
+    
+    
+    
+
+    
+
+    
+}
+
+
--- Programmatic-BMI-Calculator/Controller/CalculateViewController.swift
+++ Programmatic-BMI-Calculator/Controller/CalculateViewController.swift
@@ -2,7 +2,40 @@
 //  CalculateViewController.swift
 //  Programmatic-BMI-Calculator
 //
-//  Created by Doğukan Sakin on 6.03.2024.
+//  Created by Doğukan Sakin on 4.03.2024.
 //
 
-import Foundation
+import UIKit
+
+class CalculateViewController : UIViewController {
+
+    private var calculateView:CalculateView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupCalculateView()
+        view.backgroundColor = .white
+    }
+    
+    private func setupCalculateView(){
+        calculateView = CalculateView(frame: view.bounds)
+        calculateView.delegate = self
+        view.addSubview(calculateView)
+
+    }
+
+}
+
+extension CalculateViewController : CalculateViewDelegate {
+    func calculateBMIButtonTapped(weight: Double, height: Double) {
+        // TODO: Implement calculate func
+    }
+    
+    
+}
+
+
+@available(iOS 17.0, *)
+#Preview {
+    CalculateViewController()
+}
